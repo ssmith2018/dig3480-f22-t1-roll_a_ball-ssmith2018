f02beac9c4f88286cb5368a297d8516f5e78b213
ssmith2018 <87875779+ssmith2018@users.noreply.github.com>
2022-09-18 21:12:26 +0000

    Sep 18, 2022, 5:12 PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -36,7 +36,7 @@ public class PlayerController : MonoBehaviour
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
-        if(count >= 12)
+        if(count >= 20)
         {
             winTextObject.SetActive(true);
         }

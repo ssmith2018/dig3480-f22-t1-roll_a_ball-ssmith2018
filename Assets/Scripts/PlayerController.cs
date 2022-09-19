c6df812f15e9b9c9973776d4b4e5846233704f8e
ssmith2018 <87875779+ssmith2018@users.noreply.github.com>
2022-09-19 00:20:01 +0000

    Sep 18, 2022, 8:20 PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -78,7 +78,7 @@ public class PlayerController : MonoBehaviour
         {
             other.gameObject.SetActive(false);
             lives = lives - 1;
-            
+
             SetLivesText();
         }
         if (count == 12)
